d106ddbbdd729358f3d066ed2d9a87e8277bccc3
jwfh <me@jwfh.ca>
2019-07-03 18:14:07 -0230

    Still a few bugs in YAML parsing

--- typesetter/__init__.py
+++ typesetter/__init__.py
@@ -1,18 +1,107 @@
 #!/usr/bin/env python3
 
 
+import os
 import sys
-import subprocess
-
-
-def typeset(file: str) -> bool:
-    try:
-        subprocess.Popen()
-        return True
-    except Exception as e:
-        sys.stderr.write(str(e) + '\n')
-        return False
+import textwrap
+import yaml
+from . import template
 
 
 def run(args: list) -> None:
-    pass
+    '''
+    This, being the only public function in the module, does everything.
+    '''
+
+    # A little help message for those who don't RTFM :p
+    HELP = textwrap.dedent('''\
+    Usage: %(binary)s ARGUMENTS
+
+        [-no]-page-numbers
+                Enable/disable page numbering.
+
+        [-no]-crop-marks
+                Enable/disable visible crop marks.
+
+        -nup XDIMxYDIM
+                Enables n-up printing with XDIMxYDIM pages per sheet. Acceptable
+                values for XDIMxYDIM are '1x2', '2x2', '2x3', etc.
+
+        -h, --help
+                Prints this help message.
+    ''' % {'binary': sys.argv[0]})
+
+    # Let's try to parse the args... Yay for user input 😬
+    nup = None
+    cropMarks = True
+    pageNums = True
+    try:
+        assert len(sys.argv) > 1, textwrap.dedent('''\
+            Either I look psychic or I\'ve had one too many tonight! Why don\'t you be a
+            little more specific in telling me what you want.
+
+            ''') + HELP
+
+        argset = set(sys.argv)
+        if argset.intersection({'-h', '--help'}):
+            print(HELP)
+            sys.exit(0)
+
+        assert not {'-crop-marks', '-no-crop-marks'}.issubset(argset), \
+            'I\'m confused... Use either `-crop-marks\' or `-no-crop-marks\', not both.'
+
+        assert not {'-page-numbers', '-no-page-numbers'}.issubset(argset), \
+            'I\'m confused... Use either `-page-numbers\' or `-no-page-numbers\', not both.'
+
+        if '-nup' in sys.argv:
+            assert sys.argv.index('-nup') < len(sys.argv) - 1, \
+                'Hmm... `-nup\' requires one argument but none were given.'
+            nup = sys.argv[sys.argv.index('-nup') + 1].split('x')
+            assert len(nup) == 2, '-nup takese two arguments, delimited by an `x\'. E.g., -nup 2x2'
+        else:
+            nup = None
+
+        if '-no-crop-marks' in argset:
+            cropMarks = False
+        else:
+            # Default
+            cropMarks = True
+
+        if '-no-page-numbers' in argset:
+            pageNums = False
+        else:
+            # Default
+            pageNums = True
+
+    except AssertionError as e:
+        sys.stderr.write(str(e))
+        sys.exit(1)
+
+    recipes = dict()
+    for d in os.listdir('./recipes/'):
+        print('d', d, os.path.isdir(os.path.dirnameos.path.realpath(__file__)))
+        if os.path.isdir('../recipes/' + d):
+            liquor = os.path.basename(d)
+            print('liquor', liquor)
+            recipes[liquor] = dict()
+            for f in os.listdir('../recipes/' + d):
+                print('f', f)
+                if f.endsWith('.yaml') or f.endsWith('.yml'):
+                    recipe = yaml.safe_load(f)
+
+                    if recipe.get('name') is None:
+                        raise ValueError('Expected YAML file with `name\' defined.')
+                    if recipe.get('ingredients') is None:
+                        raise ValueError('Expected YAML file with `ingredients\' defined.')
+                    if recipe.get('instructions') is None:
+                        raise ValueError('Expected YAML file with `instructions\' defined.')
+
+                    print('Adding recipe for', recipe['name'])
+                    recipes[liquor][recipe['name']] = template.Recipe(
+                        recipe['name'],
+                        recipe['ingredients'],
+                        recipe['instructions']
+                    )
+
+    document = template.Document(recipes, pageNums, cropMarks)
+    document.typeset()
--- typesetter/template.py
+++ typesetter/template.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python3
 
 
-FILE = 'template.tex'
+import os
+import sys
+import textwrap
+import shutil
+import subprocess
+import tempfile
+
+FILE = './typesetter/template.tex'
+
+CROPMARKSTRUE = '\\usepackage[cam, width=4in, height=5.5in, center]{crop}'
+CROPMARKSFALSE = ''
+PGNUMSTRUE = '\\cfoot{\\footnotesize--\\,\\thepage\\,--}'
+PGNUMSFALSE = '\\cfoot{}'
+SECTION = '\n\\section{%(liquorName)s}\n'
+DRINK = textwrap.dedent('''\
+    \\begin{drink}{%(drinkName)s}
+        \\begin{ingredients}
+            %(ingredientsItems)s
+        \\end{ingredients}
+        \\begin{instructions}
+            %(instructionsStr)s
+        \\end{instructions}
+    \\end{drink}
+
+    ''')
 
 
-class Template:
+class Document:
     '''
     Simple class to hold a document template, the document body and
     methods to insert the latter into the former.
     '''
 
-    def __init__(self, contents: str):
+    def __init__(self, recipes: dict, pageNums: bool, cropMarks: bool):
         '''
         Creates the Template object.
 
@@ -19,15 +43,105 @@ class Template:
         fstring.
         '''
 
+        contents = ''
+        for liquor in sorted(recipes.keys()):
+            contents += SECTION % {'liquorName': liquor}
+            for recipe in sorted(recipes[liquor].keys()):
+                contents += str(recipe)
+
+        # Load the template...
         with open(FILE, 'r') as stream:
             self._template = stream.read()
 
+        # ...and populate it.
         self._content = contents
-        self._document = self._template % {'contents': self._contents}
+        self._cropMarks = CROPMARKSTRUE if cropMarks else CROPMARKSFALSE
+        self._pageNums = PGNUMSTRUE if pageNums else PGNUMSFALSE
+        self._document = self._template % {
+            'contents': self._content,
+            'cfoot': self._pageNums,
+            'cropmarks': self._cropMarks
+        }
 
-    def __str__(self):
-        return self._template % {'contents' % self._contents}
+    def __str__(self) -> str:
+        '''
+        Returns entire document as string.
+
+        Strings are immutable in Python so access to the internal string is OK.
+        '''
+
+        return self._document
 
     @property
-    def document(self):
+    def document(self) -> str:
+        '''
+        See Template.__str__.
+        '''
+
+        return str(self)
+
+    def typeset(self) -> None:
+        '''
+        Calls LatexMk to compile the PDF using LuaLaTeX.
+        '''
+        return
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            texpath = tmpdir + '/cocktails.tex'
+            pdfpath = tmpdir + '/cocktails.pdf'
+
+            with open(texpath, 'w') as texfile:
+                texfile.write(self._document)
+
+            try:
+                shutil.copyfile('./typesetter/latexmk.pl', tmpdir + '/latexmk.pl')
+                latex = subprocess.Popen(
+                    [
+                        'perl',
+                        'latexmk.pl',
+                        '-lualatex',
+                        '-halt-on-error',
+                        'cocktails'
+                    ],
+                    cwd=tmpdir
+                )
+                latex.wait()
+            except Exception as e:
+                print(
+                    'Looks like LuaLaTeX had a few too many cocktails! '
+                    'It died with the following error:\n'
+                )
+                print(str(e) + '\n')
+                sys.exit(1)
+
+            try:
+                os.rename(pdfpath, os.path.dirname(os.path.realpath(__file__)) + '/../cocktails.pdf')
+            except Exception as e:
+                print(
+                    'Looks like Python and the OS had a few too many cocktails!',
+                    'Renaming the cocktail manual failed with the following error:\n'
+                )
+                print(str(e) + '\n')
+                sys.exit(1)
+
+
+class Recipe:
+    def __init__(self, name: str, ingredients: list, instructions: str):
+        self._name = name
+        self._ingredients = ingredients
+        self._instructions = instructions
+
+    def __str__(self) -> str:
+        return DRINK % {
+            'drinkName': self._name,
+            'ingredientsItems': '\n'.join(['        \\item ' + instruction for instruction in self._instructions]),
+            'instructionsStr': self._instructions
+        }
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def recipe(self) -> str:
         return str(self)
